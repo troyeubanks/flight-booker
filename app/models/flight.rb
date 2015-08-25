a3d96b9b07ea7f409b7fd7301bff818921c3766f
Troy Eubanks <TEubanks01@gmail.com>
2015-08-24 20:52:55 -0400

    seed database with airports and random flights

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,9 @@
 class Flight < ActiveRecord::Base
+	validates :from_airport, presence: true
+	validates :to_airport,   presence: true
+	validates :depart_at,    presence: true
+	validates :duration, 		 presence: true
+
 	belongs_to :from_airport, class_name: "Airport"
 	belongs_to :to_airport, class_name: "Airport"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,13 @@ Airport.create!(airport_code: "SFO")
 Airport.create!(airport_code: "LAX")
 Airport.create!(airport_code: "NYC")
 Airport.create!(airport_code: "MCO")
+
+seed_time = Time.now
+5.times do
+	destination = rand(4) + 1
+	while (departure = rand(4) + 1) == destination
+		departure = rand(4) + 1
+	end
+	Flight.create!(to_airport_id: destination, from_airport_id: departure,
+								 depart_at: seed_time + rand(1000), duration: rand(1000))
+end
